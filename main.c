2e4e1e6cbcc68ce67cebba5964e31427c83bf4c7
luis <luis>
2002-02-06 13:32:10 +0000

    incluida la posibilidad de intriducir el numero de posiciones a mover delante de la tecla de movimiento como un numero entero decimal.

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/* $Id: main.c,v 1.8 2002/01/18 14:08:50 luis Exp $
+/* $Id: main.c,v 1.9 2002/02/06 13:32:10 luis Exp $
  * main.c -- programa principal del juego del buscaminas.
  * Autor: Luis Colorado
  * Version: 1.00 (30.1.93)
@@ -6,13 +6,14 @@
 
 #include "defs.h"
 
-static char RCS_Id[] = "\n$Id: main.c,v 1.8 2002/01/18 14:08:50 luis Exp $\n";
+static char RCS_Id[] = "\n$Id: main.c,v 1.9 2002/02/06 13:32:10 luis Exp $\n";
 
 int main (argc, argv)
 int argc;
 char *argv [];
 {
 	int c;
+	int n = 0; /* numero de posiciones a mover */
 
 	initscr ();
 	inicializa_tablero (argc, argv);
@@ -26,29 +27,37 @@ char *argv [];
 		coloca_cursor ();
 		c = getch ();
 		switch (c) {
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+			n *= 10; n += c - '0';
+			break;
 		case KEY_ESC: case KEY_BREAK:
 		case 'Q': case 'q':
 			endwin ();
 			exit (0);
 		case KEY_UP: case 'k':
-			if (pos_y == 0) break;
-			pos_y--; break;
+			pos_y -= n ? n : 1; n = 0;
+			if (pos_y < 0) pos_y = 0;
+			break;
 		case 'K':
 			pos_y = 0; break;
 		case KEY_DOWN: case 'j':
-			if (pos_y == filas - 1) break;
-			pos_y++; break;
+			pos_y += n ? n : 1; n = 0;
+			if (pos_y >= filas) pos_y = filas - 1;
+			break;
 		case 'J':
 			pos_y = filas - 1;
 			break;
 		case KEY_LEFT: case 'h':
-			if (pos_x == 0) break;
-			pos_x--; break;
+			pos_x -= n ? n : 1; n = 0;
+			if (pos_x <= 0) pos_x = 0;
+			break;
 		case 'H':
 			pos_x = 0; break;
 		case KEY_RIGHT: case 'l':
-			if (pos_x == columnas - 1) break;
-			pos_x++; break;
+			pos_x += n ? n : 1; n = 0;
+			if (pos_x >= columnas) pos_x = columnas - 1;
+			break;
 		case 'L':
 			pos_x = columnas - 1; break;
 		case KEY_ENTER: case KEY_ESP: case 'c':
@@ -61,4 +70,4 @@ char *argv [];
 	} /* for(;;) */
 } /* main */
 
-/* $Id: main.c,v 1.8 2002/01/18 14:08:50 luis Exp $ */
+/* $Id: main.c,v 1.9 2002/02/06 13:32:10 luis Exp $ */
